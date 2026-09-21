6ea31a7f11bd67d9c7b0ca33e8f36b13bc78944a
apurvas3 <apurvas3@fa26-cs598a-032.cs.illinois.edu>
2026-09-21 18:04:57 -0500

    Precompute triangle intersection coefficients to avoid per-ray solveScalers

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -9,7 +9,7 @@ Triangle::Triangle(Vector c, Vector b, Vector a, Texture* t):Plane(Vector(0,0,0)
 
    xsin = -right.z;
    if(xsin<-1.)xsin = -1;
-   else if (xsin>1.)xsin=1.; 
+   else if (xsin>1.)xsin=1.;
    yaw = asin(xsin);
    xcos = sqrt(1.-xsin*xsin);
 
@@ -34,33 +34,43 @@ Triangle::Triangle(Vector c, Vector b, Vector a, Texture* t):Plane(Vector(0,0,0)
    Vector np = solveScalers(right, up, vect, a-c);
    textureY = np.y;
    thirdX = np.x;
-   
+
    d = -vect.dot(center);
+   Vector n1 = up.cross(vect);
+   Vector n2 = vect.cross(right);
+   invD = 1.0 / right.dot(n1);
+   n1x = n1.x; n1y = n1.y; n1z = n1.z;
+   n2x = n2.x; n2y = n2.y; n2z = n2.z;
 }
 
 double Triangle::getIntersection(Ray ray){
    double time = Plane::getIntersection(ray);
-   if(time==inf) 
+   if(time==inf)
       return time;
-   Vector dist = solveScalers(right, up, vect, ray.point+ray.vector*time-center); 
-   unsigned char tmp = (thirdX - dist.x) * textureY + (thirdX-textureX) * (dist.y - textureY) < 0.0;
-   return((tmp!=(textureX * dist.y < 0.0)) || (tmp != (dist.x * textureY - thirdX * dist.y < 0.0)))?inf:time;
+   Vector C = ray.point+ray.vector*time-center;
+   double dx = (C.x*n1x + C.y*n1y + C.z*n1z) * invD;
+   double dy = (C.x*n2x + C.y*n2y + C.z*n2z) * invD;
+   unsigned char tmp = (thirdX - dx) * textureY + (thirdX-textureX) * (dy - textureY) < 0.0;
+   return((tmp!=(textureX * dy < 0.0)) || (tmp != (dx * textureY - thirdX * dy < 0.0)))?inf:time;
 }
+
 
 bool Triangle::getLightIntersection(Ray ray, double* fill){
    const double t = ray.vector.dot(vect);
    const double norm = vect.dot(ray.point)+d;
    const double r = -norm/t;
    if(r<=0. || r>=1.) return false;
-   Vector dist = solveScalers(right, up, vect, ray.point+ray.vector*r-center);
-   
-   unsigned char tmp = (thirdX - dist.x) * textureY + (thirdX-textureX) * (dist.y - textureY) < 0.0;
-   if ((tmp!=(textureX * dist.y < 0.0)) || (tmp != (dist.x * textureY - thirdX * dist.y < 0.0))) return false;
-   
-   if(texture->opacity>1-1E-6) return true;   
+   Vector C = ray.point+ray.vector*r-center;
+   double dx = (C.x*n1x + C.y*n1y + C.z*n1z) * invD;
+   double dy = (C.x*n2x + C.y*n2y + C.z*n2z) * invD;
+
+   unsigned char tmp = (thirdX - dx) * textureY + (thirdX-textureX) * (dy - textureY) < 0.0;
+   if ((tmp!=(textureX * dy < 0.0)) || (tmp != (dx * textureY - thirdX * dy < 0.0))) return false;
+
+   if(texture->opacity>1-1E-6) return true;
    unsigned char temp[4];
    double amb, op, ref;
-   texture->getColor(temp, &amb, &op, &ref,fix(dist.x/textureX-.5), fix(dist.y/textureY-.5));
+   texture->getColor(temp, &amb, &op, &ref,fix(dx/textureX-.5), fix(dy/textureY-.5));
    if(op>1-1E-6) return true;
    fill[0]*=temp[0]/255.;
    fill[1]*=temp[1]/255.;
--- src/triangle.h
+++ src/triangle.h
@@ -5,6 +5,7 @@
 class Triangle : public Plane{
 public:
    double thirdX;
+   double n1x, n1y, n1z, n2x, n2y, n2z, invD;
    Triangle(Vector c, Vector b, Vector a, Texture* t);
    double getIntersection(Ray ray);
    bool getLightIntersection(Ray ray, double* fill);
