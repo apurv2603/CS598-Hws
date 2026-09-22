c0bd6918b8bc22f32d21c9497a3963516ca6a19f
sx37 <sx37@fa26-cs598a-038.cs.illinois.edu>
2026-09-21 19:31:32 -0500

    Finalize OpenMP scheduling and add elephant scene

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ void set(int i, int j, unsigned char r, unsigned char g, unsigned char b){
 
 
 void refresh(Autonoma* c) {
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(dynamic, 16)
     for (int n = 0; n < H * W; ++n) {
         Vector ra =
             c->camera.forward
